1edb5e742e5b050851a01722e18f14b8ffe742e5
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-10-08 18:41:53 -0600

    Refactor iteration 2

--- app/models/request_type.rb
+++ app/models/request_type.rb
@@ -3,11 +3,26 @@ class RequestType < ActiveRecord::Base
   has_many :payloads
 
   def self.most_frequent
-    group(:http_verb).count.max_by{ |k, v| v}[0]
+    request_types = Payload.all.group(:request_type_id).count
+    request_type = request_types.sort_by{ |k, v| v}.last
+    RequestType.find(request_type[0]).http_verb
   end
 
   def self.verbs_list
-    group(:http_verb).count.keys.sort
+    request_types = Payload.all.group(:request_type_id).count
+    request_types_sort = request_types.sort_by{ |k, v| v}
+    thing = request_types_sort.map do |element|
+      RequestType.find(element[0]).http_verb
+    end
   end
+
+
+  # def self.where_payload(requested_verb)
+  #   Payload.where(request_type_id: find_request_type(requested_verb))
+  # end
+  #
+  # def self.find_request_type(requested_verb)
+  #   find_by(http_verb: requested_verb.to_s)
+  # end
 
 end
