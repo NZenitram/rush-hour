ecf1735be257b0434b59cc4fe3b577c90b07c9a5
Michael Scherer <michael.c.scherer@gmail.com>
2016-10-09 14:30:27 -0600

    Iteration 4 - consider refactoring

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -1,9 +1,30 @@
 module RushHour
   class Server < Sinatra::Base
+    set :show_exceptions, :after_handler
+
     not_found do
       erb :error
     end
-    
-    # Write the rest of your controller code here!
+
+    post '/sources' do
+      if  ( params[:identifier].nil? || params[:identifier] == "" ||
+            params[:rootUrl].nil?    || params[:rootUrl] == ""
+            )
+            status 400
+            body "Missing parameters\n"
+      elsif Client.exists?(identifier: params[:identifier])
+        status 403
+        body "Identifier Already Exists\n"
+       else
+          Client.find_or_create_by(
+              identifier: params[:identifier],
+              root_url: params[:rootUrl]
+              )
+          status 200
+          body "{'identifier':'#{params[:identifier]}'}\n"
+      end
+
+    end
+
   end
 end
